d134b2ea91f21de146a75212ca2c1984873c3d89
evgenydarkhanov <makaroch817@gmail.com>
2024-06-14 18:07:58 +0300

    add files

---
+++ 10/coins.py
@@ -0,0 +1,23 @@
+def exchange(coins: list[int], money: int) -> list:
+	result = []
+	i = len(coins) - 1
+	while money > 0:
+		while i > -1:
+			if money >= coins[i]:
+				money -= coins[i]
+				result.append(coins[i])
+				continue
+			else:
+				i -= 1
+				continue
+		break
+
+	return result
+	
+	
+if __name__ == "__main__":
+	print('[1, 2, 5, 10], 11 == ', exchange([1, 2, 5, 10], 11))
+	print('[1, 2, 5], 11 == ', exchange([1, 2, 5], 11))
+	print('[1, 2, 5, 10], 9 == ', exchange([1, 2, 5, 10], 9))
+	print('[2, 5, 10], 1 == ', exchange([2, 5, 10], 1))
+
